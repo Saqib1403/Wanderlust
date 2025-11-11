8e19cc094431cbaae770a55ce567984e932433b3
Saqib Ikram <saqibikram1403@gmail.com>
2025-11-11 21:04:37 +0530

    update file

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const mongo_Url = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -67,9 +66,9 @@ const sessionOption = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hi I'm root");
-// });
+app.get("/", (req, res) => {
+  res.send("Hi I'm root");
+});
 
 app.use(session(sessionOption));
 app.use(flash());
